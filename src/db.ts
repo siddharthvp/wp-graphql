17f8be12769f87bcf7cf012e9b3552e04b6eef88
Siddharth VP <siddharthvp@gmail.com>
2023-10-14 19:47:10 +0530

    use conf from a json file, clean up db.ts

--- src/db.ts
+++ src/db.ts
@@ -5,26 +5,23 @@
 
 import * as mysql from 'mysql2/promise';
 import {getCredentials, log, onToolforge, sleep} from "./utils";
+import * as conf from "./conf.json";
 
-const conf = {
-    wikiDbHost: 'enwiki.analytics.db.svc.wikimedia.cloud',
-    wikiDbPort: 3306,
-    tunnelHost: '127.0.0.1',
-    tunnelPort: 4711,
-    connectionLimit: 10,
-    inactiveConnDestroyTimeout: 5,
-    connRetryDelay: 1,
+interface ToolforgePoolConnection extends mysql.PoolConnection {
+    inactiveTimeout: NodeJS.Timeout;
 }
 
-abstract class Db {
+class Db {
     pool: mysql.Pool;
 
-    protected constructor(customOptions: mysql.PoolOptions = {}) {
+    constructor(customOptions: mysql.PoolOptions = {}) {
         this.pool = mysql.createPool({
-            port: 3306,
-            ...getCredentials('sdzerobot'),
+            host: onToolforge() ? conf.db.host : conf.db.tunnel.host,
+            port: onToolforge() ? conf.db.port : conf.db.tunnel.port,
+            database: conf.db.database,
+            ...getCredentials('wp-graphql'),
             waitForConnections: true,
-            connectionLimit: conf.connectionLimit,
+            connectionLimit: conf.db.connectionLimit,
             //timezone: 'Z',
             typeCast: function (field, next) {
                 if (field.type === 'BIT' && field.length === 1) {
@@ -40,12 +37,12 @@ abstract class Db {
         // Destroy connections on 5 seconds of inactivity. This avoids holding
         // idle connections and at the same time avoids the performance issue in
         // creating new connections for every query in a sequential set
-        this.pool.on('release', (connection) => {
+        this.pool.on('release', (connection: ToolforgePoolConnection) => {
             connection.inactiveTimeout = setTimeout(() => {
                 connection.destroy();
-            }, conf.inactiveConnDestroyTimeout * 1000);
+            }, conf.db.inactiveConnDestroyTimeout * 1000);
         });
-        this.pool.on('acquire', function (connection) {
+        this.pool.on('acquire', function (connection: ToolforgePoolConnection) {
             clearTimeout(connection.inactiveTimeout);
         });
 
@@ -56,8 +53,8 @@ abstract class Db {
         try {
             return await this.pool.getConnection();
         } catch (e) { // try again
-            log(`[W] ${e.code}: retrying in ${conf.connRetryDelay} seconds...`);
-            await sleep(conf.connRetryDelay * 1000);
+            log(`[W] ${e.code}: retrying in ${conf.db.connRetryDelay} seconds...`);
+            await sleep(conf.db.connRetryDelay * 1000);
             return await this.pool.getConnection();
         }
     }
@@ -89,17 +86,6 @@ abstract class Db {
         return [timeTaken, queryResult];
     }
 
-    async run(...args: any[]) {
-        // convert `undefined`s in bind parameters to null
-        if (args[1] instanceof Array) {
-            args[1] = args[1].map(item => item === undefined ? null : item);
-        }
-        let conn = await this.getConnection();
-        return await conn.execute(...args).finally(() => {
-            conn.release();
-        });
-    }
-
     /**
      * To be called when use of db is over.
      * All in-progress queries are executed before a quit packet is sent to mysql server.
@@ -107,29 +93,6 @@ abstract class Db {
     async end() {
         await this.pool.end();
     }
-}
-
-class EnwikiDb extends Db {
-    constructor(customOptions: mysql.PoolOptions = {}) {
-        super({
-            host: onToolforge() ? conf.wikiDbHost : conf.tunnelHost,
-            port: onToolforge() ? conf.wikiDbPort : conf.tunnelPort,
-            database: 'enwiki_p',
-            ...customOptions
-        });
-    }
-
-    // replagHours: number;
-    // replagHoursCalculatedTime: MwnDate;
-    //
-    // async getReplagHours() {
-    //     log('[V] Querying database lag');
-    //     const lastrev = await this.query(`SELECT MAX(rev_timestamp) AS ts FROM revision`);
-    //     const lastrevtime = new bot.date(lastrev[0].ts);
-    //     this.replagHours = Math.round((Date.now() - lastrevtime.getTime()) / 1000 / 60 / 60);
-    //     this.replagHoursCalculatedTime = new bot.date();
-    //     return this.replagHours;
-    // }
 }
 
 export interface SQLError extends Error {
@@ -141,4 +104,4 @@ export interface SQLError extends Error {
     sqlMessage: string;
 }
 
-export const db = new EnwikiDb();
+export const db = new Db();
