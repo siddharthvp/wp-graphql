ffe78484f8c092c512882fa634956c8e5eb0381b
Siddharth VP <siddharthvp@gmail.com>
2023-10-18 01:45:06 +0530

    WIP: normalize user-provided titles

--- src/title.ts
+++ src/title.ts
@@ -4,6 +4,6 @@ export class Title {
     constructor(namespace: number, name: string) {
         // Note: order of property names matter, due to Object.getOwnPropertyNames() usage!
         this.namespace = namespace;
-        this.name = name;
+        this.name = name.replace(/ /g, '_');
     }
 }
